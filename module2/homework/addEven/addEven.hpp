4677d6ba09b19ba658b893dae6312ed18020f027
Przemek Malec <przemyslaw.malec1997@gmail.com>
2020-06-10 20:53:10 +0200

    addEven Works

--- module2/homework/addEven/addEven.hpp
+++ module2/homework/addEven/addEven.hpp
@@ -2,7 +2,12 @@
 #include <vector>
 
 int addEven(const std::vector<int>& numbers) {
+    long int result=0;
+    for(auto&el :numbers)
+        if(!(el&1))
+            result+=el;
+    
     // TODO: Your implementation goes here
     // Below return is only to make this function compile now
-    return -1;
+    return result;
 }
