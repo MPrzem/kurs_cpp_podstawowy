b76c21f319ac089a517121291837757198b167aa
Przemek Malec <przemyslaw.malec1997@gmail.com>
2020-06-11 09:24:48 +0200

    2/5 ready

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,11 +1,36 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int lower;
+    int bigger;
+     if(lhs<0)
+        lhs=-lhs;
+    if(rhs<0)
+        rhs=-rhs;
+    
+    if(lhs>rhs){
+        bigger=lhs;
+        lower=rhs;
+        }
+    else
+    {
+        bigger=rhs;
+        lower=lhs;
+    }
+    if(lower==0||bigger==0)
+        return bigger;
+    else return NWD(lower,bigger%lower);
 }
 
+
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int nwd=NWD(lhs,rhs);
+    int result;
+    if(nwd!=0)
+    result = lhs*rhs/nwd;
+    else
+    result= 0;  
+    if(result<0) result=-result;
+    return result;
+
 }
